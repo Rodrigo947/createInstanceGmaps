f094653a542102bfc44d93b8ec947f99d15b9625
Rodrigo Torres <rodrigotorres@ice.ufjf.br>
2020-03-03 16:00:49 -0300

    verifica se os pontos sao validos

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let nGateways = Math.round((quantPontos * 20) / 100);
+let quantGateways = 0;
+let coordGateways = [];
+let coordClients = [];
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
@@ -9,6 +14,7 @@ function initMap() {
 function boundsNeighborhood() {
   $.getJSON("JSONBounds.txt", function(data) {
     finalData = data;
+    finalData[69].coordenadas = bugfix(); //Problema com o bairro de Nossa Senhora de lourdes
     finalData.forEach(function(item) {
       var polygon = new google.maps.Polygon({
         path: item.coordenadas,
@@ -22,17 +28,28 @@ function boundsNeighborhood() {
 
     var sumZones = sumPopZone();
     //console.log(testeDeRandomizacao(sumZones));
-
+    var zona, bairros, point;
     for (let index = 0; index < 1000; index++) {
-      var zona = randomZone(sumZones);
+      zona = randomZone(sumZones);
 
-      var bairros = allBairros(zona);
+      bairros = bairrosZona(zona);
 
-      escolheBairro(bairros);
+      point = gerarPonto(bairros);
+      if(quantGateways==nGateways){
+        while(!verificaPonto(point)){
+       
+          zona = randomZone(sumZones);
+
+          bairros = bairrosZona(zona);
+
+          point = gerarPonto(bairros);
+        }
+      }
+      desenhaPonto(point);
       
     }
-    
 
+    
   });
 }
 
@@ -71,25 +88,24 @@ function randomZone(sumZones) {
 }
 
 //Retorna um array com todos os bairros da zona informada
-function allBairros(zona) {
+function bairrosZona(zona) {
   let vetBairros = [];
   for (let index = 0; index < finalData.length; index++) {
-    if(finalData[index].zona == zona)
-      vetBairros.push(finalData[index]);
+    if (finalData[index].zona == zona) vetBairros.push(finalData[index]);
   }
   return vetBairros;
 }
 
 //Define um ponto em um bairro escolhido randomicamente
-function escolheBairro(bairros){
- 
+function gerarPonto(bairros) {
+  //Total de todas as zonas
   let totalpop = 0;
   for (let index = 0; index < bairros.length; index++)
     totalpop += bairros[index].populacao;
+  //----
 
+  //Verificando em qual bairro o numero gerado randomicamente está alocado
   let nRandom = Math.floor(Math.random() * (totalpop - 1 + 1)) + 1;
-
-  
   let min = 0;
   let max = bairros[0].populacao;
   let i;
@@ -98,67 +114,118 @@ function escolheBairro(bairros){
     min = max + 1;
     max += bairros[i + 1].populacao;
   }
-
+  //----
 
   var polygon = new google.maps.Polygon({
     path: bairros[i].coordenadas
   });
 
-  var marcador = new google.maps.Circle({
-    strokeColor: '#0000FF',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#0000FF',
-    fillOpacity: 0.35,
-    map: map,
-    center: randomizarLatLong(polygon, minMaxLatLong(bairros[i].coordenadas)),
-    radius: 20
-    });
-
-  marcador.setMap(map);
-
- 
-
-}
-
-//retorna o maior numero e menor valor de latitude e longitude
-function minMaxLatLong(bounds){
-  var minMax = {
-      min: {lat: 0, lng: 0},
-      max: {lat: -100, lng: -100}
-  }
-      
-  for(var i=0;i<bounds.length;i++){
-      
-      if(minMax.min.lat>bounds[i].lat) minMax.min.lat = bounds[i].lat;
-      if(minMax.min.lng>bounds[i].lng) minMax.min.lng = bounds[i].lng;
-
-      if(minMax.max.lat<bounds[i].lat) minMax.max.lat = bounds[i].lat;
-      if(minMax.max.lng<bounds[i].lng) minMax.max.lng = bounds[i].lng;
-  }
-
-  return minMax;
-}
-
-//Randomiza os valores de lat e lgn até q o mesmo esteja dentro do poligono indicado
-function randomizarLatLong(polygon,minMax){
-  var point;
-  var latitude,longitude;
- 
-  latitude = Math.random() * (minMax.max.lat - minMax.min.lat) + minMax.min.lat;
-  longitude = Math.random() *  (minMax.max.lng - minMax.min.lng) + minMax.min.lng;
-  var point =  new google.maps.LatLng(latitude, longitude);
-  
-  while(!google.maps.geometry.poly.containsLocation(point,polygon)){
-      latitude = Math.random() * (minMax.max.lat - minMax.min.lat) + minMax.min.lat;
-      longitude = Math.random() *  (minMax.max.lng - minMax.min.lng) + minMax.min.lng;
-      point = new google.maps.LatLng(latitude, longitude);
-  }
+  var point = randomizarLatLong(polygon, minMaxLatLong(bairros[i].coordenadas));
 
   return point;
 }
 
+//retorna o maior numero e menor valor de latitude e longitude
+function minMaxLatLong(bounds) {
+  var minMax = {
+    min: { lat: 0, lng: 0 },
+    max: { lat: -100, lng: -100 }
+  };
 
+  for (var i = 0; i < bounds.length; i++) {
+    if (minMax.min.lat > bounds[i].lat) minMax.min.lat = bounds[i].lat;
+    if (minMax.min.lng > bounds[i].lng) minMax.min.lng = bounds[i].lng;
+
+    if (minMax.max.lat < bounds[i].lat) minMax.max.lat = bounds[i].lat;
+    if (minMax.max.lng < bounds[i].lng) minMax.max.lng = bounds[i].lng;
+  }
+
+  return minMax;
+}
+//----
+
+//Randomiza os valores de lat e lgn até que o mesmo esteja dentro do poligono indicado
+function randomizarLatLong(polygon, minMax) {
+  var point;
+  var latitude, longitude;
+
+  latitude = Math.random() * (minMax.max.lat - minMax.min.lat) + minMax.min.lat;
+  longitude =
+    Math.random() * (minMax.max.lng - minMax.min.lng) + minMax.min.lng;
+  point = new google.maps.LatLng(latitude, longitude);
+
+  while (!google.maps.geometry.poly.containsLocation(point, polygon)) {
+    latitude =
+      Math.random() * (minMax.max.lat - minMax.min.lat) + minMax.min.lat;
+    longitude =
+      Math.random() * (minMax.max.lng - minMax.min.lng) + minMax.min.lng;
+    point = new google.maps.LatLng(latitude, longitude);
+  }
+
+  return point;
+}
+//----
+
+//Desenha de verde os primeiros 20% pontos e o restante de azul e
+//popula os vetores com os pontos criados  
+function desenhaPonto(point){
+  if (quantGateways != nGateways) {
+    quantGateways++;
+
+    var marcador = new google.maps.Circle({
+      strokeColor: "#00FF00",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#00FF00",
+      fillOpacity: 0.35,
+      map: map,
+      center: point,
+      radius: 20
+    });
+    coordGateways.push(point);
+  } else {
+    var marcador = new google.maps.Circle({
+      strokeColor: "#0000FF",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#0000FF",
+      fillOpacity: 0.35,
+      map: map,
+      center: point,
+      radius: 20
+    });
+    coordClients.push(point);
+  }
+
+  marcador.setMap(map);
+}
+
+//verifica se o ponto esta no alcance de pelo menos um gateway
+function verificaPonto(point){
+  var distancia = 0
+  for (let index = 0; index < coordGateways.length; index++) {
+    distancia = google.maps.geometry.spherical.computeDistanceBetween(
+      coordGateways[index],
+      point
+    );
+    if(distancia<=100)
+      return true;
+    
+  }
+  return false;
+    
+  /*
+  var flightPath = new google.maps.Polyline({
+    path: [coordGateways[0], coordClients[1]],
+    geodesic: true,
+    strokeColor: "#000000",
+    strokeOpacity: 1.0,
+    strokeWeight: 10
+  });
+
+  flightPath.setMap(map);
+  */
+}
 
 //Mostra a pocentagem de valores de cada zona
 //Função apenas para verificar se a randomização de zonas corresponde com a porcentagem real
@@ -177,4 +244,26 @@ function testeDeRandomizacao(sumZones) {
     console.log(key + ":" + (porcentagem[key] * 100) / 10000);
   }
   //-----------------------------------
+}
+
+//Correção das coordenadas do bairro de Nossa Senhora de Lourdes
+function bugfix() {
+  var MultiGeometryCoordinates =
+    "-43.3219525657738,-21.7606695321644,0 -43.3219175302344,-21.76086359101,0 -43.3218775730375,-21.7610022659872,0 -43.3218583210616,-21.7611269310902,0 -43.3217776419759,-21.7612455888117,0 -43.32167654968,-21.7613497128421,0 -43.3215575435913,-21.7614401123257,0 -43.3214225063686,-21.7615228326,0 -43.3212755072492,-21.7615852726183,0 -43.3210600407129,-21.7616341453527,0 -43.3208410363436,-21.7617050425665,0 -43.3208081304168,-21.7617050425665,0 -43.3206083444327,-21.7617567518799,0 -43.3203839332684,-21.7618077201399,0 -43.3201764320481,-21.7619083634827,0 -43.3200007392104,-21.7620185474314,0 -43.3198986602195,-21.7621155090542,0 -43.3198168465598,-21.7622279965273,0 -43.3197579499468,-21.7623523640099,0 -43.3197238793462,-21.7624845806081,0 -43.3197073813722,-21.7626446772865,0 -43.3196892749839,-21.7628725730459,0 -43.3196894656401,-21.7630669188156,0 -43.3196990879073,-21.763204002528,0 -43.319716164203,-21.7633404513804,0 -43.3197406503158,-21.7634759119294,0 -43.31977248284,-21.7636100332905,0 -43.3198632602331,-21.7638580303477,0 -43.3200427542822,-21.7639585845063,0 -43.3208025808066,-21.7638564978735,0 -43.321244745709,-21.764002886911,0 -43.3212817610484,-21.7643161305515,0 -43.3213241393293,-21.7646308222575,0 -43.3203567738379,-21.7650325614358,0 -43.3203341449781,-21.7658523536574,0 -43.320441649811,-21.7662601187615,0 -43.32059007381,-21.7667324492698,0 -43.3208820454178,-21.767143481917,0 -43.3210202717691,-21.767339418724,0 -43.3213246067353,-21.7678157766038,0 -43.3213678924422,-21.7681040528836,0 -43.3214819337142,-21.7682630762502,0 -43.3216040996506,-21.7684139506949,0 -43.3217361652056,-21.768560578106,0 -43.3218997945521,-21.7687040650383,0 -43.322100630985,-21.7688137131268,0 -43.3222909708818,-21.7689129769385,0 -43.3226668993895,-21.7691089166678,0 -43.3227072787745,-21.7689878159475,0 -43.3230205597189,-21.7694166519567,0 -43.3231853472283,-21.7693412427222,0 -43.3234184039231,-21.7689982117916,0 -43.3239479806278,-21.768650746832,0 -43.3249894540433,-21.7678277935398,0 -43.3248734396575,-21.7669404278316,0 -43.32520099814,-21.7663281807636,0 -43.325742559562,-21.7659076154165,0 -43.3264734277692,-21.7656137725116,0 -43.3265852518756,-21.7655347049617,0 -43.3264834104181,-21.7653547314351,0 -43.3259528097599,-21.7646290381471,0 -43.3257633449375,-21.7642874365574,0 -43.3256667440161,-21.763944365234,0 -43.3256502302495,-21.7638116973435,0 -43.3255635252511,-21.7635048623868,0 -43.3254803034126,-21.7630089483216,0 -43.3254818949103,-21.7628394468766,0 -43.3254917084651,-21.7625889599266,0 -43.3235098906933,-21.7613975075608,0 -43.3228680247386,-21.76105575791,0 -43.3219525657738,-21.7606695321644,0";
+
+  //Retorna um array de coordenadas no mesmo formato usado pelo google maps
+
+  var data = [];
+  var grouped = MultiGeometryCoordinates.split(" ");
+
+  grouped.forEach(function(item, i) {
+    let a = item.trim().split(",");
+
+    data.push({
+      lat: parseFloat(a[1]),
+      lng: parseFloat(a[0])
+    });
+  });
+
+  return data;
 }
